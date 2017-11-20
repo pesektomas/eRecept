4b7a7371a13c1cd1bbef5ba5cf3ce39d2df3e530
Tomas Pesek <pesovo@gmail.com>
2017-11-20 16:07:26 +0100

    Změna uspořádání složek

--- erecept/src/Request/Entity/Version.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-namespace eRecept\Request\Entity;
-
-class Version extends OnlyMessageEntity
-{
-
-}
--- erecept/src/Response/ZmenitPredpisResponse.php
+++
@@ -1,8 +0,0 @@
-<?php declare (strict_types = 1);
-
-namespace eRecept\Response;
-
-class ZmenitPredpisResponse extends ZalozitPredpisResponse implements Response
-{
-
-}
